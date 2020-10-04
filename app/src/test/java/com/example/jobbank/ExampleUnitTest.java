c45dc31ff99751618f373876e2f0bbc17265078c
Sanduni Galagoda <srg19981234@gmail.com>
2020-10-04 23:36:20 +0530

    Sanduni Commit 2

--- app/src/test/java/com/example/jobbank/ExampleUnitTest.java
+++ app/src/test/java/com/example/jobbank/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.example.jobbank;
 
+import android.widget.Button;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +12,24 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    view_applications v;
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void button_is_correct1(){
+        boolean a = v.checkStatus("Initial");
+        assertEquals(true, a);
+
+    }
+
+    @Test
+    public void button_is_correct2(){
+        boolean b = v.checkStatus("Rejected");
+        assertEquals(false, b);
+
+    }
+
 }
